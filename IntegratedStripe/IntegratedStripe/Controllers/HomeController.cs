7216a0fbf4a9978d4eba4e9133e74bfdbcefdee7
Deepakkumar8130 <dk0802270@gmail.com>
2024-04-17 12:10:15 +0530

    Integraeted Stripe

---
+++ IntegratedStripe/IntegratedStripe/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using IntegratedStripe.Models;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Checkout;
+using System.Diagnostics;
+
+namespace IntegratedStripe.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Route("create-checkout-session")]
+        [HttpPost]
+        public ActionResult Create()
+        {
+            StripeConfiguration.ApiKey = _config["secret"];
+
+            var domain = "http://localhost:7067";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                  new SessionLineItemOptions
+                  {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                      UnitAmount = 20400,
+                      Currency = "usd",
+                      ProductData = new SessionLineItemPriceDataProductDataOptions
+                      {
+                        Name = "T-shirt",
+                      },
+
+                    },
+                    Quantity = 1,
+                  },
+                },
+                Mode = "payment",
+                SuccessUrl = domain,
+                CancelUrl = domain + "/Home/Privacy",
+            };
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            Response.Headers.Add("Location", session.Url);
+            return Redirect(session.Url);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
